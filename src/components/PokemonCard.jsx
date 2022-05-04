fa3cd651bda19a0a06633be57f9877371ff70fb2
João Marcos <jmarcossferreira@hotmail.com>
2022-05-04 19:50:20 -0300

    Improving layout component

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-const PokemonCard = ({ pokemon, animate = false }) => {
+const PokemonCard = ({ pokemon }) => {
   return (
     <Link href={`/details/${pokemon.id}`}>
       <div
         className={`px-4 py-8 rounded-md shadow-lg cursor-pointer 
                   bg-gray-50 flex flex-col items-center justify-center
-                  ${animate && "hover:scale-105"} transition-all`}
+                  hover:scale-105 transition-all`}
       >
         <Image
           height={120}
